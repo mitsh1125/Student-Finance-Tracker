abcbb4437c9b08a863b313867ceede4d53e0c4cc
potatofrieees-ui <mssyoooooooo@gmail.com>
2025-05-01 15:23:16 +0800

    student finance

--- app/src/main/java/com/example/mystudentfinancetrackerapp/login.java
+++ app/src/main/java/com/example/mystudentfinancetrackerapp/login.java
@@ -13,7 +13,12 @@ public class login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
+
+
+
         setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
